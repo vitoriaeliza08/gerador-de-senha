3aeae2df0b292f7eceef46003db7f34e9b4b3d62
vitoriaeliza08 <vitoria.sgarbossa.santos@escola.pr.gov.br>
2025-05-14 12:17:07 -0300

    vi

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector('.forca');
